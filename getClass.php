38aff2e371342abff02807c87faeebbf8bbf97b4
drose379 <dylanrose60@Dylan-Roses-MacBook.local>
2014-10-27 17:41:58 -0400

    getClass now grabs all info from resume array

--- getClass.php
+++ getClass.php
@@ -5,6 +5,8 @@ class Resume {
 	protected $Tele;
 	protected $Location = array();
 	protected $Education = array();
+	protected $Experience = array();
+	protected $Skills = array();
 
 
 public function __construct(array $fullInfo) {
@@ -12,8 +14,11 @@ public function __construct(array $fullInfo) {
 	$this->Tele = $fullInfo['Tele'];
 	$this->Location = $fullInfo['Location'];
 	$this->Education = $fullInfo['Education'];
+	$this->Experience = $fullInfo['Work Experience'];
+	$this->Skills = $fullInfo['Skills'];
 }
 
+//All Getters
 public function getName() {
 	return $this->Name;
 }
@@ -30,6 +35,12 @@ public function getEdu() {
 	return $this->Education;
 }
 
+public function getExp() {
+	return $this->Experience;
+}
 
+public function getSkills() {
+	return $this->Skills;
+}
 
 }
