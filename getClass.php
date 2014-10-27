04fa6f887cb0a84d8f8a63300793953780cc63c2
drose379 <dylanrose60@Dylan-Roses-MacBook.local>
2014-10-27 17:29:42 -0400

    added education to getClass

--- getClass.php
+++ getClass.php
@@ -4,12 +4,14 @@ class Resume {
 	protected $Name;
 	protected $Tele;
 	protected $Location = array();
+	protected $Education = array();
 
 
 public function __construct(array $fullInfo) {
 	$this->Name = $fullInfo['Name'];
 	$this->Tele = $fullInfo['Tele'];
 	$this->Location = $fullInfo['Location'];
+	$this->Education = $fullInfo['Education'];
 }
 
 public function getName() {
@@ -22,6 +24,10 @@ public function getTele() {
 
 public function getLocation() {
 	return $this->Location;
+}
+
+public function getEdu() {
+	return $this->Education;
 }
 
 public function GeneralGet() {
