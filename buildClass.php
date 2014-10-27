bda2d17e786d32b49c28d59e82af6ec021243bf1
drose379 <dylanrose60@DylanRosMacBook.home>
2014-10-26 21:46:05 -0400

    fix array to string issue

--- buildClass.php
+++ buildClass.php
@@ -12,7 +12,9 @@ public function buildHeader() {
 $header = array( "Name" => $this->resume->getName(), "Telephone" => $this->resume->getTele(), "Location" => $this->resume->getLocation());
 
 foreach ($header as $key => $data) {
+	if (is_string($data)) {
 	$string[] = $key . $data;
+}
 
 if ( is_array($data) ) {
 	foreach ( $data as $specific ) {
